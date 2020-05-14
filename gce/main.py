08327748f5f3526c72d9a293a1422fcbf45efce2
jiff <inajiff@gmail.com>
2020-05-14 13:21:54 -0700

    remove pasted in remainder of logger

--- gce/main.py
+++ gce/main.py
@@ -106,12 +106,6 @@ if app.debug:
 log.info('2.... Logging setup')
 
 
-# Retrieves a Cloud Logging handler based on the environment
-# you're running in and integrates the handler with the
-# Python logging module. By default this captures all logs
-# at INFO level and higher
-client.get_default_handler()
-client.setup_logging()
 
 
 def allowed_file(filename):
