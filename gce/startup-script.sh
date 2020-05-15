1cd65611aacaf1e619e8fe2b3efc28187342e6cf
jiff <inajiff@gmail.com>
2020-05-15 14:41:58 -0700

    running java install

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -1,20 +1,26 @@
-# Install Stackdriver logging agent
-curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
-sudo bash install-logging-agent.sh
-
-# Install or update needed software
-apt-get update
-apt-get install -yq git supervisor python3-pip apt-transport-https software-properties-common
-#pip install --upgrade pip virtualenv
-pip3 --version
-pip3 install virtualenv
+# sudo tee /etc/google-fluentd/config.d/test-unstructured-log.conf <<EOF
+# <source>
+#     @type tail
+#     # Format 'none' indicates the log is unstructured (text).
+#     format none
+#     # The path of the log file.
+#     path /tmp/test-unstructured-log.log
+#     # The path of the position file that records where in the log file
+#     # we have processed already. This is useful when the agent
+#     # restarts.
+#     pos_file /var/lib/google-fluentd/pos/test-unstructured-log.pos
+#     read_from_head true
+#     # The log tag for this log input.
+#     tag unstructured-log
+# </source>
+# EOF
 sudo tee /etc/google-fluentd/config.d/test-unstructured-log.conf <<EOF
 <source>
     @type tail
     # Format 'none' indicates the log is unstructured (text).
     format none
     # The path of the log file.
-    path /tmp/test-unstructured-log.log
+    path /tmp/gunicorn.error.log
     # The path of the position file that records where in the log file
     # we have processed already. This is useful when the agent
     # restarts.
@@ -24,14 +30,29 @@ sudo tee /etc/google-fluentd/config.d/test-unstructured-log.conf <<EOF
     tag unstructured-log
 </source>
 EOF
-sudo service google-fluentd reload
+# Install Stackdriver logging agent
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
 
-# Install Allure
-sudo apt-add-repository ppa:qameta/allure
-sudo add-apt-repository ppa:webupd8team/java
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt-get update && apt-get install -y allure
+# Install or update needed software
+apt-get update
+apt-get install -yq git supervisor python3-pip apt-transport-https software-properties-common
+pip3 --version
+pip3 install virtualenv
+
+# Install Java & Allure
+sudo apt-add-repository ppa:qameta/allure -y
+sudo add-apt-repository ppa:linuxuprising/java -y
+echo debconf shared/accepted-oracle-license-v1-2 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-2 seen true | sudo debconf-set-selections
+sudo apt update -y
+sudo apt install oracle-java14-installer -y
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+sudo apt-get update -y && sudo apt-get install allure  -y
+echo "Allure version is $(allure --version)"
+
+
 
 # Account to own server process
 useradd -m -d /home/pythonapp pythonapp
